927676eee9f2cd75d9872d676f2bf21d38e00fa1
nicejjss <locdaoduc2002@gmail.com>
2022-08-17 12:21:11 +0700

    add

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 
 console.log(1);
-
-setTimeout(time,2000,"Hello",123,321);
-
-console.log(3);
-
-
-function time(x,y){
-  console.log("This is call back: "+x+" and "+y);
-}
-
-const request= new XMLHttpRequest();
-
-request.open("GET","https://jsonplaceholder.typicode.com/todos");
-
-request.send()
+console.log("nicejjss");
